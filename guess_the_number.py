fe0de498148ca9e6c6da31f0bbdce0e52d59b603
A Muse <museaynan@gmail.com>
2015-01-14 12:03:56 +0000

    Update guess_the_number.py
    Guess The Number program updated!

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,6 @@
 import random
 print("Hello! Today we are going to play Guess The Number!")
-number = int(raw_input("Guess a number between 0 and 5"))
+number = int(input("Guess a number between 0 and 5"))
 random_number = random.randint(0, 5)
 if number == random_number:
     print("You won!")
@@ -8,4 +8,4 @@ if number == random_number:
     print("Nothing!")
 else:
     print("You lost!")
-    print("The number was %d") % (random_number)
+    print("The number was {0}.".format(random_number))
