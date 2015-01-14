de5cce4f6b6dd57e67b3787cd51c589ac971c6ba
A Muse <museaynan@gmail.com>
2015-01-14 09:46:40 +0000

    Create guess_the_number.py
    Created 'Guess The Number' game in Python

---
+++ guess_the_number.py
@@ -0,0 +1,11 @@
+import random
+print("Hello! Today we are going to play Guess The Number!")
+number = int(raw_input("Guess a number between 0 and 5"))
+random_number = random.randint(0, 5)
+if number == random_number:
+    print("You won!")
+    print("Your prize is...")
+    print("Nothing!")
+else:
+    print("You lost!")
+    print("The number was %d") % (random_number)
